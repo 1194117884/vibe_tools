c3bc804ab7a1f690bf6f412b1463d8b98b20302e
yongkl <yongk_liu@163.com>
2026-04-26 18:54:47 +0800

    add a new logo

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Inter+Tight:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
       </Head>
       <body>
         <Main />
